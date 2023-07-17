0fbc9483d6cfe22d8a9e56ff87b2db7c65eab37e
Emmanuel Yelisomah <yelisomahe@gmail.com>
2023-07-17 14:22:56 +0000

    My 0x00-hello_world directory

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 1 (Success)
  */
-int main(void){
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+int main(void)
+{
+	write("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
 
